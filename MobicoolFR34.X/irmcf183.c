0ca78e404f4572907d93d261a71f5b8f7b8eb724
Werner Johansson <wj@unifiedengineering.se>
2018-07-04 23:45:29 +0200

    Rework compressor speed setting
    Turns out that only certain speeds were accepted, map these and
    refer to them by index instead

--- MobicoolFR34.X/irmcf183.c
+++ MobicoolFR34.X/irmcf183.c
@@ -18,6 +18,11 @@
 #include "mcc_generated_files/mcc.h"
 #include "irmcf183.h"
 
+// Only the following speeds seems to be recognized, all others are silently set to ~65
+static const uint8_t s_supported_speeds[] = { 3, 6, 9, 17, 19, 22, 25, 33, 35, 38, 41,
+                                              49, 51, 54, 57, 65, 67, 70, 73, 81, 83 };
+#define NUM_SPEEDS (sizeof(s_supported_speeds) / sizeof(s_supported_speeds[0]))
+
 static bool s_compressor_on = false;
 
 static void UART_Xmit(uint8_t* buf, uint8_t len) {
@@ -35,14 +40,29 @@ void Compressor_Init(void) {
     UART_Xmit( (uint8_t[]){0xe1, 0xeb, 0x90, 0x00, 0x00, 0x00, 0x00}, 7);
 }
 
-void Compressor_OnOff(bool on, bool fanon, uint8_t speed) {
+void Compressor_OnOff(bool on, bool fanon, uint8_t speedidx) {
     IO_DCDCEna_LAT = fanon;
     IO_FanEna_LAT = fanon;
     IO_Comp12VCtrl_LAT = fanon;
-    UART_Xmit( (uint8_t[]){0xe1, 0xeb, 0x90, on ? 1 : 0, speed, 0x00, 0x00}, 7);
+    uint8_t newspeed = 0;
+    if (speedidx >= NUM_SPEEDS) speedidx = NUM_SPEEDS - 1;
+    newspeed = s_supported_speeds[speedidx];
+    UART_Xmit( (uint8_t[]){0xe1, 0xeb, 0x90, on ? 1 : 0, newspeed, 0x00, 0x00}, 7);
     s_compressor_on = on;
 }
 
 bool Compressor_IsOn(void) {
     return s_compressor_on;
+}
+
+uint8_t Compressor_GetMinSpeedIdx(void) {
+    return 7;
+}
+
+uint8_t Compressor_GetMaxSpeedIdx(void) {
+    return NUM_SPEEDS - 1;
+}
+
+uint8_t Compressor_GetDefaultSpeedIdx(void) {
+    return 10;
 }
--- MobicoolFR34.X/irmcf183.h
+++ MobicoolFR34.X/irmcf183.h
@@ -2,7 +2,10 @@
 #define	IRMCF183_H
 
 void Compressor_Init(void);
-void Compressor_OnOff(bool on, bool fanon, uint8_t speed);
+void Compressor_OnOff(bool on, bool fanon, uint8_t speedidx);
 bool Compressor_IsOn(void);
+uint8_t Compressor_GetMinSpeedIdx(void);
+uint8_t Compressor_GetMaxSpeedIdx(void);
+uint8_t Compressor_GetDefaultSpeedIdx(void);
 
 #endif	/* IRMCF183_H */
